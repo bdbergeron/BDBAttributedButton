2c7150bdd741385955516c27ffe06102ddcabbf5
Bradley David Bergeron <bradbergeron@gmail.com>
2014-01-15 20:53:58 -0500

    1.0.0

---
+++ BTButtonDemo/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTButtonDemo
+//
+//  Created by Bradley Bergeron on 12/1/14.
+//  Copyright (c) 2014 Bradley David Bergeron. All rights reserved.
+//
+
+@import UIKit;
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-BTButton
-========
+# BTButton
+
+BTButton is a UIButton subclass for iOS 7. With BTButton, you can create round buttons and rounded rect buttons with ease.
